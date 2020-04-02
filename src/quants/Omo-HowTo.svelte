6e4b0ba9900c2c81f3169507ec9d55c009820f09
Philipp Bartels <philipp.bartels@logon-consulting.de>
2020-04-02 07:23:50 +0200

    3 steps wrong

---
+++ src/quants/Omo-HowTo.svelte
@@ -0,0 +1,60 @@
+<div class="flex justify-center my-20">
+  <div class="w-5/6 xl:w-4/6">
+    <img src="/images/divider-1.png" alt="divider" class="px-48 mt-8" />
+    <h2 class="text-center text-5xl text-ci mt-10 mb-6">UNSERE TRÄUME</h2>
+    <div class="text-2xl text-gray-600 text-center mb-16">
+      <p>"In 3 Schritten kannst du alles ändern"</p>
+    </div>
+
+    <div class="flex content-start flex-wrap">
+      <div class="w-1/3 p-2">
+        <a href="chat">
+          <div class="primary omo-border overflow-hidden omo-shadow">
+            <img
+              src="https://images.unsplash.com/photo-1519337718347-749509f114a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+              class="primary h-64 w-full rounded-t-lg object-cover object-center"
+              alt="image" />
+            <div
+              class="text-xl text-center p-4 text-gray-200 mb-2 bg-ci-2 flex
+              flex-wrap justify-center content-center h-48">
+              <p>
+                Chatte mit Robin und erfasse deine Daten ohne nerviges Formular
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="w-1/3 p-2">
+        <a href="chat">
+          <div class="primary omo-border overflow-hidden omo-shadow">
+            <img
+              src="https://images.unsplash.com/photo-1582578598774-a377d4b32223?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
+              class="primary h-64 w-full rounded-t-lg object-cover object-center"
+              alt="image" />
+            <div
+              class="text-xl text-center p-4 text-gray-200 mb-2 bg-ci-2 flex
+              flex-wrap justify-center content-center h-48">
+              <p>Finde deinen nächsten Gesprächspartner</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="w-1/3 p-2">
+        <a href="chat">
+          <div class="primary omo-border overflow-hidden omo-shadow">
+            <img
+              src="https://images.unsplash.com/photo-1489367874814-f5d040621dd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1329&q=80"
+              class="primary h-64 w-full rounded-t-lg object-cover object-center"
+              alt="image" />
+            <div
+              class="text-xl text-center p-4 text-gray-200 mb-2 bg-ci-2 flex
+              flex-wrap justify-center content-center h-48">
+              <p>Habt eine Menge Spaß zusammen!</p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,6 +5,7 @@
   import OmoThemeLight from "../design/Omo-Theme-Light.svelte";
   import OmoBlog from "../quants/Omo-Blog.svelte";
   import OmoCities from "../quants/Omo-Cities.svelte";
+  import OmoHowTo from "../quants/Omo-HowTo.svelte";
   import OmoHeader from "../quants/Omo-Header.svelte";
   import OmoDreams from "../quants/Omo-Dreams.svelte";
   import OmoCallToAction from "../quants/Omo-CallToAction.svelte";
@@ -13,6 +14,7 @@
 </script>
 
 <OmoHeader {db} {currentId} />
+<OmoHowTo />
 <OmoDreams {db} {currentId} />
 <!-- <OmoCallToAction {db} {currentId} /> -->
 <OmoEnkels {db} {currentId} />
