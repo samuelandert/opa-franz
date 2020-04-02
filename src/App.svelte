b496b220f335f6b1e317c626cc5195f9ba527f62
Philipp Bartels <philipp.bartels@logon-consulting.de>
2020-04-02 03:06:22 +0200

    Cities added

--- src/App.svelte
+++ src/App.svelte
@@ -29,12 +29,17 @@ const db = {
     },
     {
       id: 1,
-      name: "Heidelbörg",
+      name: "Heidelberg",
       image: "https://source.unsplash.com/Yfo3qWK2pjY/250x250"
     },
     {
       id: 2,
       name: "Berlin",
+      image: "https://source.unsplash.com/pN684G33h_M/250x250"
+    },
+     {
+      id: 3,
+      name: "Memmingen",
       image: "https://source.unsplash.com/pN684G33h_M/250x250"
     }
   ],
@@ -45,7 +50,7 @@ const db = {
       image: "https://source.unsplash.com/rDEOVtE7vOs/500x500",
       story:
         "Eines der schönsten Momente meines Lebens habe ich mit Onkel Josef-Dieter gehabt",
-      city: "Berlin"
+      city: 2
     },
     {
       id: 1,
@@ -53,7 +58,7 @@ const db = {
       image: "https://source.unsplash.com/7YVZYZeITc8/500x500",
       story:
         "Eines der schönsten Momente meines Lebens habe ich mit Onkel Josef-Dieter gehabtf",
-      city: "Heidelberg"
+      city: 1
     },
     {
       id: 2,
@@ -61,7 +66,7 @@ const db = {
       image: "https://source.unsplash.com/rriAI0nhcbc/500x500",
       story:
         "Eines der schönsten Momente meines Lebens habe ich mit Onkel Josef-Dieter gehabt",
-      city: "Berlin"
+      city: 2
     },
     {
       id: 3,
@@ -69,7 +74,7 @@ const db = {
       image: "https://source.unsplash.com/xe68QiMaDrQ/500x500",
       story:
         "Eines der schönsten Momente meines Lebens habe ich mit Onkel Josef-Dieter gehabt",
-      city: "Memmingen"
+      city: 3
     }
   ],
   dreams: [
@@ -78,14 +83,16 @@ const db = {
       name: "Opa Jakob",
       dream: "ist es als ältester Mensch einen Marathon zu laufen",
       freuden: "spielen",
-      color: "bg-ci-2"
+      color: "bg-ci-2",
+      city:1
     },
     {
       id: 2,
       name: "Tante Inge",
       dream: "es noch einmal an die Nordsee zu fahren",
       freuden: "spazieren",
-      color: "bg-ci"
+      color: "bg-ci",
+      city:2
     },
 
     {
@@ -93,7 +100,8 @@ const db = {
       name: "Onkel Otto",
       dream: "ist es ins Stadion des Champions League Finales gehen",
       freuden: "puzzeln",
-      color: "bg-ci-2"
+      color: "bg-ci-2",
+      city:3
     }
   ],
   blog:[
@@ -103,7 +111,8 @@ const db = {
       subtitle: "eine wundersame Begegnung",
       excerpt:
         "Letzte Woche haben sich zum ersten mal zwicshen Alf und Emmy ein Generationen Tandem gebildet. Sie waren gemeinam auf dem Weg zum Schachspielen als es geschah...",
-      image: "https://source.unsplash.com/random?sig=123/800x800/"
+      image: "https://source.unsplash.com/random?sig=123/800x800/",
+      cities: [1,2,3]
     },
     {
       id: 2,
@@ -111,7 +120,8 @@ const db = {
       subtitle: "die ersten Tage von Opa Franz",
       excerpt:
         "Peter, Philipp und Samuel hatten eines Tages den Geistesblitz Opa Franz für Memmingen zu entwickeln. Innerhalb von 24 Stunden bauten sie gemeinsam den ersten...",
-      image: "https://source.unsplash.com/random?sig=321/800x800/"
+      image: "https://source.unsplash.com/random?sig=321/800x800/",
+      cities: [1,2,3]
     }
   ]
 	};
--- src/routes/City.svelte
+++ src/routes/City.svelte
@@ -3,6 +3,27 @@
   export let db;
   export let currentId;
   let city = db.cities.find(item=>item.id==currentId);
+  let enkels = db.enkels.filter(item=>item.city==city.id);
+  let dreams = db.dreams.filter(item=>item.city==city.id);
+  let blog = db.blog.filter(item=>item.cities.some(id=>id==city.id));
+  debugger
 </script>
 
-<OmoCity {city} />
+<h1>{city.name}</h1>
+<br><br>
+<h2>Enkels</h2>
+{#each enkels as enkel}
+    <pre>{ JSON.stringify(enkel) }</pre>
+{/each}
+<br>
+<h2>Opas</h2>
+{#each dreams as dream}
+    <pre>{ JSON.stringify(dream) }</pre>
+{/each}
+<br>
+
+<h2>Blog</h2>
+{#each blog as b}
+    <pre>{ JSON.stringify(b) }</pre>
+{/each}
+<br>
