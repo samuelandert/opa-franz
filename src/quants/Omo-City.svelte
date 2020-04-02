40516511392f85565b4289d2a2cb3a415d6d4ef0
Philipp Bartels <philipp.bartels@logon-consulting.de>
2020-04-02 02:44:44 +0200

    design

--- src/quants/Omo-City.svelte
+++ src/quants/Omo-City.svelte
@@ -1,4 +1,7 @@
-<script>export let city;</script>
+<script>
+export let city;
+</script>
+
 <a class="w-1/4 p-2" href="cities?id={city.id}">
   <div class="primary omo-border overflow-hidden omo-shadow">
     <div class="h-64 bg-cover bg-center overflow-hidden flex justify-center
